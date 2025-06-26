584d3d9843d1cefc715e127dc7efff2fb0c7d0dd
xanderdrvr <xander.deroover@student.ucll.be>
2025-06-26 12:17:56 +0200

    Layered Architecture: UserRepository + UserService file creation

---
+++ backend/src/main/java/com/nucleuspass/service/UserService.java
@@ -0,0 +1,17 @@
+package com.nucleuspass.service;
+
+import com.nucleuspass.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
+}
