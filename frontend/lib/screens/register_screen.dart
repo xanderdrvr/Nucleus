6bf6cc3a3728b7e79c952fe967134270fd1b6be1
xanderdrvr <xander.deroover@student.ucll.be>
2025-07-03 13:10:04 +0200

    Layout: Logo on home page, file structure for authentication, start global styling

---
+++ frontend/lib/screens/register_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+        backgroundColor: const Color.fromARGB(255, 21, 23, 44),
+      ),
+      body: const Center(child: Text('Register Screen')),
+    );
+  }
+}
